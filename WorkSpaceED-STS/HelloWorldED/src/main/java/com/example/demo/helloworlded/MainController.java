d7a40596302c1243a1c4608be3eae47fb60f4776
ZaidP6 <pilarads06@gmail.com>
2024-04-23 22:20:19 +0200

    controller actualizado y hecho rebase

--- WorkSpaceED-STS/HelloWorldED/src/main/java/com/example/demo/helloworlded/MainController.java
+++ WorkSpaceED-STS/HelloWorldED/src/main/java/com/example/demo/helloworlded/MainController.java
@@ -1,18 +1,24 @@
 package com.example.demo.helloworlded;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
 	@GetMapping("/")
-	public String sayHello() {
+	public String sayHello(Model model) {
 
+		model.addAttribute("mensaje", "Hola mundo, CHAVALADA");
 		return "index";
 	}
+<<<<<<< HEAD
 	
 }
 
 //comprobacion de que se sube el commit en la rama adecuada
 //ya se ha creado la rama02 en el remoto, se subiran bien los cambios ahora?
+=======
+}
+>>>>>>> a581368 (controller actualizado)
