a581368d5572a335956c7f775c0f2a437a2d8ba8
ZaidP6 <pilarads06@gmail.com>
2024-04-23 22:20:19 +0200

    controller actualizado

--- WorkSpaceED-STS/HelloWorldED/src/main/java/com/example/demo/helloworlded/MainController.java
+++ WorkSpaceED-STS/HelloWorldED/src/main/java/com/example/demo/helloworlded/MainController.java
@@ -1,15 +1,16 @@
 package com.example.demo.helloworlded;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
 	@GetMapping("/")
-	public String sayHello() {
+	public String sayHello(Model model) {
 
+		model.addAttribute("mensaje", "Hola mundo, CHAVALADA");
 		return "index";
 	}
-	
 }
