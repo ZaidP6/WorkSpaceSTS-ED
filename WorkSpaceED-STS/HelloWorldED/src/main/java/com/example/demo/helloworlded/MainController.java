e1fbfb0927d20b762760a90e3234e42dd511a392
ZaidP6 <pilarads06@gmail.com>
2024-04-23 23:30:49 +0200

    estoy en la main

--- WorkSpaceED-STS/HelloWorldED/src/main/java/com/example/demo/helloworlded/MainController.java
+++ WorkSpaceED-STS/HelloWorldED/src/main/java/com/example/demo/helloworlded/MainController.java
@@ -1,24 +1,19 @@
 package com.example.demo.helloworlded;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
 	@GetMapping("/")
-	public String sayHello(Model model) {
+	public String sayHello() {
 
-		model.addAttribute("mensaje", "Hola mundo, CHAVALADA");
 		return "index";
 	}
-<<<<<<< HEAD
 	
 }
 
 //comprobacion de que se sube el commit en la rama adecuada
 //ya se ha creado la rama02 en el remoto, se subiran bien los cambios ahora?
-=======
-}
->>>>>>> a581368 (controller actualizado)
+// se supone q ya estoy en la rama main y he hecho rebase, porq no me deja subir? porq pide un git pull?
