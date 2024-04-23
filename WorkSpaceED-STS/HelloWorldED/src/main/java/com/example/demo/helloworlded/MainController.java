bdce3a00c4f3f971f84e694b4dbe47458ccf0e6b
ZaidP6 <pilarads06@gmail.com>
2024-04-23 21:20:30 +0200

    class MainController añadida

--- WorkSpaceED-STS/HelloWorldED/src/main/java/com/example/demo/helloworlded/MainController.java
+++ WorkSpaceED-STS/HelloWorldED/src/main/java/com/example/demo/helloworlded/MainController.java
@@ -2,14 +2,13 @@ package com.example.demo.helloworlded;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 @Controller
 public class MainController {
 
 	@GetMapping("/")
 	public String sayHello() {
+
 		return "index";
 	}
 	
